12c31cb84ad214a610b6bf4ff079310a551c66e8
dannyTH <dannymaish80@gmail.com>
2018-05-04 16:07:14 +0300

    Add changes made on scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var personInput= $("input#input1").val();
-  })
-})
+    $("#potray").empty();
+    var personInput= parseInt($("input#input1").val());
+    var potray= listDown(personInput);
+
+    potray.forEach (function(item) {
+      $("#potray").append ('<li>' + item + '</li>');
+
+    });
+    event.preventDefault();
+  });
+});
+
+var listDown= function(personInput) {
+  var result= [];
+  for(var i=1; i<=personInput; i++) {
+    var index= result.indexOf(i);
+    if (i % 15 ===0) {
+      result.splice (index, 0);
+      result.push('pingpong');
+    } else if (i % 3 ===0) {
+      result.splice(index, 0);
+      result.push('ping');
+    }else if (i % 5 ===0) {
+      result.splice(index, 0);
+      result.push('pong');
+    }else {
+    result.push(i);
+    }
+  }
+  return result;
+};
